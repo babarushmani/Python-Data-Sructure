0969fb6749d2ef97bbaedb002becff92b9cb035a
babarushmani <babar.ushmani@gmail.com>
2024-01-13 01:34:35 +0530

    Add files via upload

---
+++ power x to n .py
@@ -0,0 +1,12 @@
+# Write a program get x pov n.
+
+
+# get input from user
+
+base = int(input("type a no as base and it enter:"))
+pov = int(input("enter a no as pov of base:"))
+
+result = base**pov
+
+print(result)
+
